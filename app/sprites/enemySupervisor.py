2fe5fb93ee94f8aa790dbff95f8369b005021fa8
Benoit-Pouliot <pouliot.benoit@gmail.com>
2016-12-10 16:17:22 -0500

    bug, spawn

--- app/sprites/enemySupervisor.py
+++ app/sprites/enemySupervisor.py
@@ -14,13 +14,13 @@ class EnemySupervisor():
 
 
         for listData in self.mapData.enemyGenListData.listData:
-            if listData[0] > self.mapData.internalMapTime:
+            if listData[0] < self.mapData.internalMapTime:
                 for k in range(listData[2]):
 
                     randEnemyGen = random.randint(0,len(self.listEnemyGenerator)-1)
                     randTypeIDEnemy = random.choice(listData[1])
                     self.listEnemyGenerator[randEnemyGen].spawnEnemy(self.mapData.enemyGenListData.dictListEnemiesValues[randTypeIDEnemy])
 
-            if listData[3] > 0:
-                self.mapData.enemyGenListData.addData(listData[0]+listData[3],listData[1],listData[2],listData[3])
-            self.mapData.enemyGenListData.listData.pop(0)
+                if listData[3] > 0:
+                    self.mapData.enemyGenListData.addData(listData[0]+listData[3],listData[1],listData[2],listData[3])
+                self.mapData.enemyGenListData.listData.pop(0)
